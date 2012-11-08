542887ddd9625bc66183dbfd5938e45e2dcdc9bf
nmlgc <nmlgc@thpatch.net>
2012-11-08 18:31:47 +0100

    Initial version ($wgTitle->getPageLanguage()->mCode)

---
+++ PageLang.i18n.magic.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Internationalization file for NumberOfWikis extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$magicWords = array();
+
+/** English (English) */
+$magicWords['en'] = array(
+	'pagelang' => array( 0, 'pagelang' ),
+);
---
+++ PageLang.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Number of wikis -- a magic word to show the number of wikis on ShoutWiki
+ *
+ * @file
+ * @ingroup Extensions
+ * @version 0.2
+ * @date March 3, 2011
+ * @author Jack Phoenix <jack@shoutwiki.com>
+ * @license http://en.wikipedia.org/wiki/Public_domain Public domain
+ */
+
+if( !defined( 'MEDIAWIKI' ) ) {
+	die( "This is not a valid entry point.\n" );
+}
+
+// Extension credits that will show up on Special:Version
+$wgExtensionCredits['variable'][] = array(
+	'name' => 'Page language',
+	'author' => 'Nmlgc',
+	'description' => 'Defines a variable that contains the current page content language',
+);
+
+// Translations for {{pagelang}}
+$dir = dirname( __FILE__ ) . '/';
+$wgExtensionMessagesFiles['PageLangMagic'] = $dir . 'PageLang.i18n.magic.php';
+
+$wgHooks['ParserGetVariableValueSwitch'][] = 'wfPageLangAssignValue';
+function wfPageLangAssignValue( &$parser, &$cache, &$magicWordId, &$ret ) {
+    if ( 'pagelang' == $magicWordId ) {
+            // We found a value
+            global $wgTitle;
+            if($wgTitle) {
+	            $ret = $wgTitle->getPageLanguage()->mCode;
+	        }
+            /*if( 'ja' == $ret) {
+            	$ret = "";
+            }*/
+    }
+    return true;
+}
+
+$wgHooks['MagicWordwgVariableIDs'][] = 'wfPageLangVariableIds';
+function wfPageLangVariableIds( &$variableIds ) {
+	$variableIds[] = 'pagelang';
+	return true;
+}
